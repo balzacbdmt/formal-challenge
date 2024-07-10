ec299aa73cec583629e18688255f39456478422d
Balzac <balzac.baudemont@gmail.com>
2024-07-10 14:40:46 +0200

    gradient bg

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import Home from "./views/home/Home";
 
 function App() {
   return (
-    <>
+    <div className="h-screen bg-gradient-to-b from-slate-100 to-indigo-200">
       <Home />
-    </>
+    </div>
   );
 }
 
