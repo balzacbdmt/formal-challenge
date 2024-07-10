694759fbdbb767d0a45b049f143e1ceb1c177217
Balzac <balzac.baudemont@gmail.com>
2024-07-10 15:21:17 +0200

    structure home page + creating card component

---
+++ src/views/home/Card/Card.tsx
@@ -0,0 +1,41 @@
+import { Icon } from "@iconify/react";
+import { format, formatDistanceToNow } from "date-fns";
+
+interface Props {
+  title: string;
+  updatedAt: number;
+  logsCount: number;
+  dataName: string;
+  // TODO data prop
+  dataUpdatedAt?: number;
+}
+
+function Card({ title, updatedAt, logsCount, dataName, dataUpdatedAt }: Props) {
+  return (
+    <div className="p-6 text-gray-400 bg-slate-100 w-fit rounded-2xl flex gap-8">
+      <div className="flex flex-col justify-between">
+        <div>
+          <h2>{title}</h2>
+          <p>Updated {formatDistanceToNow(updatedAt, { addSuffix: true })}</p>
+        </div>
+        {logsCount > 0 && (
+          <div className="flex items-center gap-1 mb-3.5">
+            <Icon icon="mdi:alert-circle-outline" />
+            <span className="text-xs">{logsCount} New Logs since update</span>
+          </div>
+        )}
+      </div>
+      <div className="h-48 w-96 bg-white rounded-xl p-4 flex flex-col justify-between">
+        <p>{dataName}</p>
+        <p>TODO Graph or stats</p>
+        {dataUpdatedAt && (
+          <p className="text-xs">
+            {format(new Date(dataUpdatedAt), "MMM d, HH:mm:ss")}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,8 +1,13 @@
+import Card from "./Card/Card";
+
 function Home() {
+
   return (
-    <>
-      <h1>home page</h1>
-    </>
+    <div className="px-12 py-16">
+      <h1 className="font-medium mb-2">Recent</h1>
+      <Card title="Logs" updatedAt={1720616586000} logsCount={12} dataName="Logs / Time" dataUpdatedAt={1720616586000} />
+      <h1 className="font-medium mt-8 mb-2">Saved</h1>
+    </div>
   );
 }
 
