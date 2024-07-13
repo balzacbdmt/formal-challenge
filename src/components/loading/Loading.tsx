b730663eb1debaca88390b2e30e5eed55edf532b
Balzac <balzac.baudemont@gmail.com>
2024-07-13 12:36:20 +0200

    create loading component

---
+++ src/components/loading/Loading.tsx
@@ -0,0 +1,11 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+
+function Loading() {
+  return (
+    <div className="flex flex-col items-center">
+      <Icon icon="eos-icons:loading" fontSize={44} />
+    </div>
+  );
+}
+
+export default Loading;
